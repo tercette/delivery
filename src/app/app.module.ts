5eda02225e8953b62a7b505c4cc5d929d5c84d93
tercette <leandro.tercette@gmail.com>
2022-01-27 20:55:40 -0300

    header,  footer e pages componentes created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularModule } from './shared/angular/angular.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [			
+    AppComponent,
+      HeaderComponent,
+      FooterComponent,
+      PagesComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
